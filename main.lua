938d3c1d9d88d6d8b2e2151564eba43caf3c8c40
InfinitySnapz <infinitysnaps@yahoo.com>
2021-09-18 21:36:04 +0100

    Update main.lua
    baseline

--- main.lua
+++ main.lua
@@ -1,5 +1,13 @@
 function love.load()
     love.graphics.setNewFont(12)
     love.graphics.setColor(0,0,0)
-    love.graphics.setBackgroundColor(255,255,255)
+    love.graphics.setBackgroundColor(0,0,0)
+ end
+
+ function love.update(dt)
+--
+ end
+
+ function love.draw()
+    love.graphics.print('pppong build 2')
  end
